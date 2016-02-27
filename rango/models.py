81736bc3bf0279ba99f57bf583c106db0bb8e128
Jack <jackberreby@gmail.com>
2016-02-27 18:11:56 +0000

    added some stuff

--- rango/models.py
+++ rango/models.py
@@ -5,8 +5,7 @@ class Account(models.Model):
     user = models.OneToOneField(User)
     userfn = models.CharField(max_length=20)
     userln = models.CharField(max_length=20)
-    userid = models.IntegerField(default=0)
-    balance =  models.IntegerField(unique=True,default=0)
+    balance =  models.IntegerField(default=0)
 
 
 
--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import *
+from django.contrib.auth.decorators import login_required
 
 def bienvenue(request):
     return render(request, 'base.html')
 
+@login_required
 def profile(request):
+
     return render(request, 'registration/login.html')
